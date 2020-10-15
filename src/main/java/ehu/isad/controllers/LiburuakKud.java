fe0820f8e1fbbc0cb2d083e55a7f61864594234a
adeiarias <adeiariasupv@gmail.com>
2020-10-15 21:19:45 +0200

    Klase guztien eta xfml-en sorketa

---
+++ src/main/java/ehu/isad/controllers/LiburuakKud.java
@@ -0,0 +1,25 @@
+package ehu.isad.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class LiburuakKud {
+
+    @FXML
+    private Label lbl1;
+
+    @FXML
+    private ComboBox<?> comboHautatu;
+
+    @FXML
+    private Button buttonIkusi;
+
+    @FXML
+    public void onClick(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/ehu/isad/controllers/XehetasunakKud.java
@@ -0,0 +1,43 @@
+package ehu.isad.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class XehetasunakKud {
+
+    @FXML
+    private Label lblIzenburu;
+
+    @FXML
+    private Label lblArgitaletxe;
+
+    @FXML
+    private Label lblOrriKop;
+
+    @FXML
+    private Label lblIrudi;
+
+    @FXML
+    private Label izenburua;
+
+    @FXML
+    private Label argitaletxea;
+
+    @FXML
+    private Label orriKop;
+
+    @FXML
+    private ImageView irudia;
+
+    @FXML
+    private Button buttonAtzera;
+
+    @FXML
+    public void onClick(ActionEvent event) {
+
+    }
+
+}
